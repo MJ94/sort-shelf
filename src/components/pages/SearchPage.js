4e21563eaf96068e3c7ea4c98986d6cc3b456320
Michael Jackson <mj94@mj94.net>
2018-10-31 15:40:23 -0500

    Add Link to root from SearchPage

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchPage extends React.Component {
   render () {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
