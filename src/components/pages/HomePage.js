5ad4aea4ce6fec8804b2b54b92d8517891a3f363
Michael Jackson <mj94@mj94.net>
2018-10-31 18:11:35 -0500

    Remove search code and add Link to SearchPage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,31 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class HomePage extends React.Component {
   render () {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>Sort Shelf</h1>
@@ -182,7 +161,7 @@ class HomePage extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )}
