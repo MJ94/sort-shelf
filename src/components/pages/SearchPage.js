c8144bbe5cf1fb998d4a58f54c688649e0236f16
Michael Jackson <mj94@mj94.net>
2018-11-01 17:39:12 -0500

    Begin implementing search

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,7 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI.js'
+import {getAll} from '../../BooksAPI.js'
 
 class SearchPage extends React.Component {
+  state = {
+    books: [],
+    query: "",
+    results: []
+  }
+}
+
+async componentDidMount() {
+  try {
+    const books = await getAll();
+    this.setState({
+      books
+    });
+  } catch(e) {
+      console.log(e);
+  }
+}
+
+  updateBook = (book, shelf) => {
+      BooksAPI.update(book, shelf)
+      .then(_resp => {
+          book.shelf = shelf;
+          this.setState(state => ({
+          books: state.books.filter(books => books.id !== book.id).concat([book])
+      }));
+      });
+  };
+
+  updateQuery = (query) => {
+    this.setState((query: query), this.submitSearch)
+  }
+
+  submitSearch() {
+    if (this.state.query === "" || this.state.query === undefined) {
+      return this.setState({results: []});
+    }
+  }
+
   render () {
     return (
       <div className="search-books">
@@ -16,7 +56,10 @@ class SearchPage extends React.Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
 
           </div>
         </div>
