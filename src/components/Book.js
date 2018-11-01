28e9a5c57df858a28cda8d491e476b4a341d50fa
Michael Jackson <mj94@mj94.net>
2018-11-01 15:10:41 -0500

    Change values so they're not hardcoded

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 class Book extends React.Component {
+  componentDidMount() {
+    console.log(this);
+  }
   render () {
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select value={this.props.book.shelf || "None"}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -17,8 +20,8 @@ class Book extends React.Component {
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{this.props.book.title || "No title found"}</div>
+          <div className="book-authors">{this.props.book.authors[0] || "No author found"}</div>
         </div>
       </li>
     );
