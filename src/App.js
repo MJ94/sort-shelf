3237f7c8f0343c0d1c52c45c3abbeabb5d9fc8bf
Michael Jackson <mj94@mj94.net>
2018-10-31 21:54:55 -0500

    Add <Switch> from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './components/pages/HomePage.js'
 import SearchPage from './components/pages/SearchPage.js'
@@ -8,8 +8,10 @@ import SearchPage from './components/pages/SearchPage.js'
 const App = () => {
   return (
     <div>
-      <Route exact path="/" component={ HomePage } />
-      <Route exact path="/search" component={ SearchPage } />
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/search" component={ SearchPage } />
+      </Switch>
     </div>
   );
 };
