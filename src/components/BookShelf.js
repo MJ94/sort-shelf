d0054a6aa99ad8dbb43d93354d489d265e9a86d1
Michael Jackson <mj94@mj94.net>
2018-11-01 14:44:48 -0500

    Render books based on which Bookshelf they belong to

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -8,7 +8,9 @@ class BookShelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <Book/>
+          {
+            this.props.books.map((book, key) => <Book book={book} key={key} />)
+          }
           </ol>
         </div>
       </div>
