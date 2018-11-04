eaafd9514efff832fa1f96dfde852975a32b472c
Michael Jackson <mj94@mj94.net>
2018-11-04 10:31:07 -0600

    fix: "None" -> "none" to match value

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,7 +7,7 @@ const Book = (props) => {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks.thumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select value={props.book.shelf || "None"} onChange={(event) => { props.updateBook(props.book, event.target.value) }}>
+            <select value={props.book.shelf || "none"} onChange={(event) => { props.updateBook(props.book, event.target.value) }}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
