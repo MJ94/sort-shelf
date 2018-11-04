9516eaed92a98d5843bcfd84435165b5b5659fdc
Michael Jackson <mj94@mj94.net>
2018-11-04 10:27:44 -0600

    Convert to stateless functional component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,28 +1,26 @@
 import React from 'react'
 
-class Book extends React.Component {
-  render () {
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
-            <div className="book-shelf-changer">
-              <select value={this.props.book.shelf || "None"} onChange={(event) => { this.props.updateBook(this.props.book, event.target.value) }}>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+const Book = (props) => {
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-shelf-changer">
+            <select value={props.book.shelf || "None"} onChange={(event) => { props.updateBook(props.book, event.target.value) }}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-          <div className="book-title">{this.props.book.title || "No title found"}</div>
-          <div className="book-authors">{this.props.book.authors[0] || "No author found"}</div>
         </div>
-      </li>
-    );
-  }
+        <div className="book-title">{props.book.title || "No title found"}</div>
+        <div className="book-authors">{props.book.authors[0] || "No author found"}</div>
+      </div>
+    </li>
+  );
 }
 
 export default Book;
