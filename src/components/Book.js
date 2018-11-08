83bdfc1b3eae7d66f0ee12ea7519a5abb50f103e
Michael Jackson <mj94@mj94.net>
2018-11-07 21:29:39 -0600

    Update error handling for cover rendering

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,7 +5,7 @@ const Book = (props) => {
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks ? props.book.imageLinks.thumbnail : "https://dummyimage.com/128x193/2e7c31/fff.png&text=Cover+Missing"})` }}></div>
           <div className="book-shelf-changer">
             <select value={props.book.shelf || "none"} onChange={(event) => { props.updateBook(props.book, event.target.value) }}>
               <option value="move" disabled>Move to...</option>
