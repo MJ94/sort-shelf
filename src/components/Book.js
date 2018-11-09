8853215083b99a76d51da0c660464a0879c47fb1
Michael Jackson <mj94@mj94.net>
2018-11-09 15:52:10 -0600

    Further destructuring

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Book = (props) => {
+const Book = ({ updateBookShelf, book }) => {
   return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks ? props.book.imageLinks.thumbnail : "https://dummyimage.com/128x193/2e7c31/fff.png&text=Cover+Missing"})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : "https://dummyimage.com/128x193/2e7c31/fff.png&text=Cover+Missing"})` }}></div>
           <div className="book-shelf-changer">
-            <select value={props.book.shelf || "none"} onChange={(event) => { props.updateBookShelf(props.book, event.target.value) }}>
+            <select value={book.shelf || "none"} onChange={(event) => { updateBookShelf(book, event.target.value) }}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -16,8 +16,8 @@ const Book = (props) => {
             </select>
           </div>
         </div>
-        <div className="book-title">{props.book.title || "No title found"}</div>
-        <div className="book-authors">{props.book.authors ? props.book.authors[0] : "No author found"}</div>
+        <div className="book-title">{book.title || "No title found"}</div>
+        <div className="book-authors">{book.authors ? book.authors[0] : "No author found"}</div>
       </div>
     </li>
   );
